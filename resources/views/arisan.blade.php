b186ba69df811ab8a3b74a899da6b22f08d7c53d
achmadrachmandika <119827535+achmadrachmandika@users.noreply.github.com>
2024-11-02 19:14:21 +0700

    arisan

---
+++ resources/views/arisan.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing Page</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
+
+        .hero {
+            background: url('https://source.unsplash.com/random/1200x800') no-repeat center center;
+            background-size: cover;
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            color: black;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="hero">
+        <div class="container">
+            <h1>Manajemen Arisan</h1>
+            <p>Deadline Januari namun target desember</p>
+        </div>
+    </div>
+
+</body>
+    
+</html>
